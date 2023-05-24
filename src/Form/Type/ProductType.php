e38373406e58cd63104c6d79bd56ba7f48cc53e8
StanAubert <41484171+StanAubert@users.noreply.github.com>
2023-05-24 14:45:45 +0200

    adding category select

--- src/Form/Type/ProductType.php
+++ src/Form/Type/ProductType.php
@@ -1,8 +1,10 @@
-<?php 
+<?php
 
 namespace App\Form\Type;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,17 +14,21 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class ProductType extends AbstractType
 {
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('name', TextType::class)
-            ->add('Description', TextType::class)
-            ->add('Picture', TextType::class)
+            ->add('description', TextType::class)
             ->add('price', NumberType::class)
-            ->add('save', SubmitType::class)
+            ->add('picture', TextType::class)
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name'
+            ])
+            ->add('Save', SubmitType::class, ['label' => 'Ajouter un produit'])
         ;
     }
+
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
